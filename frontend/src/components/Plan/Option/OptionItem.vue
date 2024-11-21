ae772680d8a98f5e4e145aa7bcd6bec37aed2d9b
heemin728 <idkhm0728@naver.com>
2024-11-22 01:28:39 +0900

    refactor : 빈 이미지 수정

--- frontend/src/components/Plan/Option/OptionItem.vue
+++ frontend/src/components/Plan/Option/OptionItem.vue
@@ -13,7 +13,7 @@ import FullHeart from "@/assets/image/full-heart.png";
 import EmptyHeart from "@/assets/image/empty_heart.png";
 import OptionDialog from "./OptionDialog.vue";
 
-// import NoImage from "@/assets/image/no-image.png";
+import NoImage from "@/assets/image/no-image.png";
 // import OptionDialog from "./OptionDialog.vue";
 
 const dStore = dayStore();
