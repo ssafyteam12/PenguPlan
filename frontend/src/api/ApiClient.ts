77c7a1b792fed223a9dd114da5e257be8ca5b796
김연수 <rladustn1002@gmail.com>
2024-11-22 11:32:29 +0900

    feat: interceptor 추가
    - 요청 보낼 때마다, 헤더에 accessToken포함

--- frontend/src/api/ApiClient.ts
+++ frontend/src/api/ApiClient.ts
@@ -7,4 +7,17 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
